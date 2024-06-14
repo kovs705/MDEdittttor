0689e2def305b7ca1fa7dd73d6f90da96da8ea2a
Eugene Kovs <smartkovsik@gmail.com>
2024-06-14 23:24:27 +0300

    Added SwiftUI support

---
+++ Sources/MDEdittttor/View/MDEdittttorUI.swift
@@ -0,0 +1,53 @@
+//
+//  MDEdittttorUI.swift
+//  MDEdittttor
+//
+//  Created by Eugene Kovs on 14.06.2024.
+//  https://github.com/kovs705
+//
+
+import SwiftUI
+import UIKit
+
+@available(iOS 15.0, *)
+public struct MDEdittttorWrapper: UIViewRepresentable {
+    public typealias UIViewType = MDEdittttor
+    
+    @Binding public var text: String
+    
+    public init(text: Binding<String>) {
+        self._text = text
+    }
+    
+    public func makeUIView(context: Context) -> MDEdittttor {
+        let editor = MDEdittttor.defaultMarkdownTextView()
+        editor.delegate = context.coordinator
+        return editor
+    }
+    
+    public func updateUIView(_ uiView: MDEdittttor, context: Context) {
+        if uiView.text != text {
+            uiView.text = text
+        }
+    }
+    
+    public func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    public class Coordinator: NSObject, UITextViewDelegate {
+        var parent: MDEdittttorWrapper
+        
+        init(_ parent: MDEdittttorWrapper) {
+            self.parent = parent
+        }
+        
+        public func textViewDidChange(_ textView: UITextView) {
+            parent.text = textView.text
+        }
+        
+        public func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+            return (textView as? MDEdittttor)?.textView(textView, shouldChangeTextIn: range, replacementText: text) ?? true
+        }
+    }
+}
