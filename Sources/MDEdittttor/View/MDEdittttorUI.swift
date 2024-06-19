c6e7d4ec39a11b4a6e703740728d920c207a2381
Eugene Kovs <smartkovsik@gmail.com>
2024-06-19 13:56:58 +0300

    (Test) dynamic height

--- Sources/MDEdittttor/View/MDEdittttorUI.swift
+++ Sources/MDEdittttor/View/MDEdittttorUI.swift
@@ -14,6 +14,7 @@ public struct MDEdittttorWrapper: UIViewRepresentable {
     public typealias UIViewType = MDEdittttor
     
     @Binding public var text: String
+    @State private var dynamicHeight: CGFloat = .zero
     
     public init(text: Binding<String>) {
         self._text = text
@@ -23,12 +24,17 @@ public struct MDEdittttorWrapper: UIViewRepresentable {
         let editor = MDEdittttor.defaultMarkdownTextView()
         editor.delegate = context.coordinator
         editor.isScrollEnabled = false
+        editor.setContentCompressionResistancePriority(.defaultLow, for: .vertical)
         return editor
     }
     
     public func updateUIView(_ uiView: MDEdittttor, context: Context) {
         if uiView.text != text {
             uiView.text = text
+        }
+        
+        DispatchQueue.main.async {
+            self.dynamicHeight = uiView.contentSize.height
         }
     }
     
@@ -45,6 +51,10 @@ public struct MDEdittttorWrapper: UIViewRepresentable {
         
         public func textViewDidChange(_ textView: UITextView) {
             parent.text = textView.text
+            // Update the height based on content size
+            DispatchQueue.main.async {
+                self.parent.dynamicHeight = textView.contentSize.height
+            }
         }
         
         public func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
@@ -54,7 +64,9 @@ public struct MDEdittttorWrapper: UIViewRepresentable {
 }
 
 @available(iOS 15.0, *)
-#Preview(body: {
-    @State var text = "\(ExampleText.hello)"
-    return MDEdittttorWrapper(text: $text)
-})
+struct MDEdittttorWrapper_Previews: PreviewProvider {
+    static var previews: some View {
+        @State var text = "\(ExampleText.hello)"
+        return MDEdittttorWrapper(text: $text)
+    }
+}
